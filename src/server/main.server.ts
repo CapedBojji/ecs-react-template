df331d3597ece1464325b665295bedae3bea811a
Laud Boateng <capedbojji@gmail.com>
2024-05-02 14:23:18 +0000

    May 2, 2024, 10:23 AM

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,3 +1,9 @@
-import { makeHello } from "shared/module";
+import Tree from "shared/third_party/tree";
+import start from "./start";
+import { ReplicatedStorage } from "@rbxts/services";
 
-print(makeHello("main.server.ts"));
+start(
+	[Tree.Find(script.Parent!, "systems")!, Tree.Find(ReplicatedStorage, "Shared/systems")!],
+	[Tree.Await(script.Parent!, "middlewares/before"), Tree.Await(ReplicatedStorage, "Shared/middlewares/before")],
+	[Tree.Await(script.Parent!, "middlewares/after"), Tree.Await(ReplicatedStorage, "Shared/middlewares/after")],
+);
