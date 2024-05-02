77ccba7d2251f8ca18267e31224d904f0128cf1e
Laud Boateng <capedbojji@gmail.com>
2024-05-02 14:19:32 +0000

    May 2, 2024, 10:19 AM

---
+++ src/shared/systems/test-shared-system.ts
@@ -0,0 +1,25 @@
+import { Registry } from "@rbxts/cmdr";
+import { Producer } from "@rbxts/reflex";
+import { $print } from "rbxts-transform-debug";
+import { SharedState } from "shared/store";
+import { SharedSystem } from "shared/types";
+
+const system: SharedSystem = {
+	init: () => {
+		$print(`Shared System Init`);
+	},
+
+	update: (
+		dt: number,
+		frameNumber: number,
+		args_0: Registry,
+		args_1: Producer<SharedState>,
+		args_2: Map<string, unknown>,
+	) => {
+		$print(`Hello, World!`);
+	},
+
+	toString: () => "Test Shared System",
+};
+
+export = system;
